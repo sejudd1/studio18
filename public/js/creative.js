4428b892197aa53fb25f772664d3b878a25ed2e9
ajchan11 <ajchan11@gmail.com>
2015-12-08 10:59:19 -0800

    strip

--- public/js/creative.js
+++ public/js/creative.js
@@ -7,42 +7,15 @@
 (function($) {
     "use strict"; // Start of use strict
 
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
+    $(document).ready(function() {
+        $('#fullpage').fullpage({
+            verticalCentered: true,
+            sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#4BBFC3'],
+            afterRender: function(){
+
+                //playing the video
+                $('video').get(0).play();
+            }
+        });
     });
-
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    })
-
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
-        $('.navbar-toggle:visible').click();
-    });
-
-    // Fit Text Plugin for Main Header
-    $("h1").fitText(
-        1.2, {
-            minFontSize: '35px',
-            maxFontSize: '65px'
-        }
-    );
-
-    // Offset for Main Navigation
-    $('#mainNav').affix({
-        offset: {
-            top: 100
-        }
-    })
-
-    // Initialize WOW.js Scrolling Animations
-    new WOW().init();
-
 })(jQuery); // End of use strict
