7ddb590ffdd974b4747f03bf6ad16a1aa4a8ed01
ajchan11 <ajchan11@gmail.com>
2015-12-08 15:29:17 -0800

    testing

---
+++ public/js/scrolling-nav.js
@@ -0,0 +1,26 @@
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+
+    $(document).ready(function() {
+        $('#fullpage').fullpage({
+            anchors: ['onePage', 'twoPage', 'threePage', 'fourPage', 'fivePage', 'sixPage', 'sevenPage'],
+            menu: '#menu',
+            scrollingSpeed: 1000,
+            verticalCentered: true,
+            afterRender: function(){
+
+                //playing the video
+                $('video').get(0).play();
+            }
+        });
+    });
+});
